3996ae6ca5f8697eab6b5b5c728d941c5525c9b3
Firefly4266 <gaines.johnp@gmail.com>
2017-09-24 19:07:00 -0700

    Problem 34 completed

---
+++ Inheritance/Problem34.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Problem34
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Greetings user, welcome to our program.");
+            SimpleShape shape = new SimpleShape(5);
+            Console.Write("\n The default settings for the square are ");
+            shape.ShowDim();
+            Console.Write("\n User, please enter a new number: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            SimpleShape newShape = new SimpleShape(num);
+            Console.Write("\n The default settings for the new square are ");
+            newShape.ShowDim();
+        }
+    }
+}
---
+++ Inheritance/SimpleShape.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class SimpleShape
+    {
+        double pri_width;
+        double pri_height;
+
+        public SimpleShape()
+        {
+            width = 0;
+            height = 0;
+        }
+        public SimpleShape(double num)
+        {
+            width = num;
+            height = num;
+        }
+        public double width
+        {
+            get
+            {
+                return pri_width;
+            }
+            set
+            {
+                pri_width = value;
+            }
+        }
+        public double height
+        {
+            get
+            {
+                return pri_height;                
+            }
+            set
+            {
+                pri_height = value;
+            }
+        }
+
+        public void ShowDim()
+        {
+            Console.WriteLine($" Width: {width}, Height: {height}.\n");
+        }
+    }
+}
